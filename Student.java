0eb9bb0418fb88a8ac31edc7539a78c1630a310c
Dhruv <80347912+dhruvprasanna@users.noreply.github.com>
2024-02-21 12:50:26 +0530

    first commit

--- Student.java
+++ Student.java
@@ -1,5 +1,22 @@
 public class Student {
     String name;
     int age;
+
+    public Student(String name, int age, boolean isEligible, String college) {
+        this.name = name;
+        this.age = age;
+        this.isEligible = isEligible;
+        this.college = college;
+    }
+
     boolean isEligible;
+
+    String college;
+
+    public Student(String name, int age, boolean isEligible) {
+        this.name = name;
+        this.age = age;
+        this.isEligible = isEligible;
+    }
+
 }
